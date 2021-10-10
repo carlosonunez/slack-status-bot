b88f72b08732cf04505e8b1e0c02847c1e42ccd6
Carlos Nunez <carlosonunez@noreply.github.com>
2021-10-10 17:51:37 -0500

    Fetch calendars.

--- lib/slack_status_bot/receivers/google/calendar.rb
+++ lib/slack_status_bot/receivers/google/calendar.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'google/apis/calendar_v3'
+require 'slack_status_bot/logging'
 require 'slack_status_bot/authenticators/google'
 
 module SlackStatusBot
@@ -13,9 +14,17 @@ module SlackStatusBot
         SCOPES = [
           ::Google::Apis::CalendarV3::AUTH_CALENDAR_EVENTS_READONLY
         ].freeze
+
+        # Retrieves a calendar from the list of calendars available to the
+        # Google account specified by GOOGLE_CLIENT_ID_JSON in the environment.
+        def self.resolve_from_env
+          SlackStatusBot.logger.error('GOOGLE_CALENDAR_NAME is not defined') if ENV['GOOGLE_CALENDAR_NAME'].nil?
+          return unless ENV['GOOGLE_CALENDAR_NAME'].nil?
+        end
+
         def self.events
           credentials = SlackStatusBot::Authenticators::Google.get_or_create_credentials!(SCOPES)
-          true
+          {}
         end
       end
     end
--- spec/unit/receivers/google/calendar_spec.rb
+++ spec/unit/receivers/google/calendar_spec.rb
@@ -4,22 +4,42 @@ require 'spec_helper'
 require 'google/apis/calendar_v3'
 require 'slack_status_bot/authenticators/google'
 
+# rubocop:disable Metrics/BlockLength
 describe 'Given a status receiver for Google Calendar' do
-  context 'When we are not authenticated' do
-    example 'Then we authenticate first', :unit do
-      expected_scopes = [
-        Google::Apis::CalendarV3::AUTH_CALENDAR_EVENTS_READONLY
-      ]
-      allow(SlackStatusBot::Authenticators::Google)
-        .to receive(:credentials)
-        .and_return(nil)
-      allow(SlackStatusBot::Authenticators::Google)
-        .to receive(:generate_tokens_or_raise!)
-        .and_return({})
-      expect(SlackStatusBot::Authenticators::Google)
-        .to receive(:generate_tokens_or_raise!)
-        .with(expected_scopes, 'default').once
-      _ = SlackStatusBot::Receivers::Google::Calendar.events
+  context 'When the name of a calendar is provided in the environment' do
+    before do
+      @scopes = [Google::Apis::CalendarV3::AUTH_CALENDAR_EVENTS_READONLY]
+      # rubocop:disable Lint/ConstantDefinitionInBlock
+      Calendar = SlackStatusBot::Receivers::Google::Calendar
+      # rubocop:enable Lint/ConstantDefinitionInBlock
+    end
+    context 'And our environment is not configured' do
+      example 'Then the matching Calendar object is not retrieved', :unit do
+        expect { Calendar.resolve_from_env }
+          .to output(/GOOGLE_CALENDAR_NAME is not defined/)
+          .to_stdout_from_any_process
+        expect(Calendar.resolve_from_env).to be_nil
+      end
+    end
+    context 'And our environment is configured' do
+      before do
+        ENV['GOOGLE_CALENDAR_NAME'] = 'fake-calendar'
+      end
+      after do
+        ENV['GOOGLE_CALENDAR_NAME'] = nil
+      end
+      context 'And the calendar name matches a real calendar' do
+        example 'Then the matching Calendar object can be retrieved', :unit do
+          allow(SlackStatusBot::Authenticators::Google)
+            .to receive(:get_or_create_credentials!)
+            .and_return({ access_token: 'fake', refresh_token: 'fake' })
+          calendar = Calendar.resolve_from_env
+          expect(calendar.id).to be 'fake-id'
+        end
+      end
+      context 'And the calendar name does not match a real calendar' do
+      end
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
